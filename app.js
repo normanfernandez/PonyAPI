08b5c89592c0dbf7310a78ebcde5720dfe601652
Norman <el.norman01@gmail.com>
2017-10-08 09:46:19 -0400

    Added APIs with GET and POST

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+var ponies = [{
+	name : "Twiligth Sparkle",
+	ponyStyle : {
+		color : "purple"
+	}
+}];
 
 var port = process.env.PORT || 3000;
 
@@ -8,7 +16,17 @@ app.listen(port, () => {
 });
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/public/index.html");
-})
+});
+
+app.get('/api/pony', (req, res) => {
+	res.send(ponies);
+});
+
+app.post('/api/pony', (req, res) => {
+	ponies.push(req.body);
+	res.end(JSON.stringify(res.body), null, 2);
+});
